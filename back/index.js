c4eb4b2275a9d06dd77d7e25ed5bb094ccfdde8a
BlackPawsStudio <blackpawsstudio@gmail.com>
2021-10-06 22:11:33 +0300

    update chat methods

--- back/index.js
+++ back/index.js
@@ -9,36 +9,48 @@ const usersArr = []
 let kickCounter = 0
 let voices = 0
 let victim = null
+let game = {
+  players: [],
+  master: null,
+  rounds: [],
+  currentRound: null
+}
+
+const idGenerator = (a, arr) => arr.map(e => e.id).includes(a) ? idGenerator(++a, arr) : a
 
 io.on('connection', socket => {
   console.log(`connected`)
+
   socket.on('create-user', name => {
+    name.id = idGenerator(0, usersArr)
     users[socket.id] = name
-    usersArr.push(name) 
+    usersArr[name.id] = name
     io.sockets.emit('user-connected', name)
     console.log(`${users[socket.id]} connected`)
   })
+
   socket.on('message-send', message => {
-    socket.broadcast.emit('recieving-messages', [users[socket.id], message])
+    io.sockets.emit('recieving-messages', [users[socket.id], message])
     console.log(`${users[socket.id]} messaged ${message}`)
-  })//ok
-  socket.on('get-users-request', () => {
-    socket.emit('send-all-users', usersArr)
-  })//ok
-  socket.on('user-want-to-kick-user', () => {         //убрать передаваемые на серв параметры, потому что здесь они пока не надо
-    socket.emit('kick-users-list', usersArr)
   })
+  
+  socket.on('request-players', () => {
+    socket.emit('recieve-players', usersArr)
+  })
+
   socket.on('disconnect', () => {
     socket.broadcast.emit('user-disconnected', users[socket.id])
     console.log(`${users[socket.id]} disconnected`)
     usersArr.splice(usersArr.indexOf(users[socket.id]), 1)
     delete users[socket.id]
-  })//ok
+  })
+
   socket.on('user-want-to-kick-user', name => {
     socket.broadcast.emit('kick-offer', name)
     victim = name
     console.log(`${users[socket.id]} wants to kick ${victim}`)
   })
+
   socket.on('user-opinion', answ => {
     kickCounter += 1
     if (answ) {
@@ -60,8 +72,25 @@ io.on('connection', socket => {
       victim = null
     }
   })
+
   socket.on('user-is-typing', () => {
     console.log(`Somebody is typing`)
     socket.broadcast.emit('this-user-is-typing')
+  })
+
+  socket.on('request-master', () => {
+    socket.emit('recieve-master', usersArr.find(el => { if ( el.role == "master" ) return el }))
+  })
+
+  socket.on('set-master', user => {
+    user.role = 'master'
+    users[socket.id] = user
+    usersArr[user.id] = userx
+  })
+
+  socket.on('game-start', () => {
+    game.players = usersArr;
+    game.master = usersArr.find(el => { if ( el.role == "master" ) return el })
+    socket.emit('game-started', game)
   })
 })
