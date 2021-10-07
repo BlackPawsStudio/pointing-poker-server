52f07e9b1699b3917646b680b8c7553bac940af0
BlackPawsStudio <blackpawsstudio@gmail.com>
2021-10-07 12:40:25 +0300

    add consoling

--- back/index.js
+++ back/index.js
@@ -34,7 +34,7 @@ io.on('connection', (socket) => {
 
   socket.on('message-send', (message) => {
     io.sockets.emit('recieving-messages', [users[socket.id], message])
-    console.log(`${users[socket.id]} messaged ${message}`)
+    console.log(users[socket.id], 'messaged ', message)
   })
   
   socket.on('request-players', () => {
@@ -44,7 +44,7 @@ io.on('connection', (socket) => {
 
   socket.on('disconnect', () => {
     socket.broadcast.emit('user-disconnected', users[socket.id])
-    console.log(`${users[socket.id]} disconnected`)
+    console.log(users[socket.id], 'disconnected')
     usersArr.splice(usersArr.indexOf(users[socket.id]), 1)
     delete users[socket.id]
   })
@@ -52,24 +52,24 @@ io.on('connection', (socket) => {
   socket.on('user-want-to-kick-user', (name) => {
     socket.broadcast.emit('kick-offer', name)
     victim = name
-    console.log(`${users[socket.id]} wants to kick ${victim}`)
+    console.log(users[socket.id],'wants to kick', victim)
   })
 
   socket.on('user-opinion', (answ) => {
     kickCounter += 1
     if (answ) {
-      console.log(`${users[socket.id]} chosen ${answ}`)
+      console.log(users[socket.id], 'chosen', answ)
       voices += 1
     }
     console.log(`all voices ${kickCounter}, voices agrees ${voices}`)
     if (usersArr.length == kickCounter) {
       if (voices >= usersArr.length / 2) {
         socket.broadcast.emit('force-disconnect', victim)
-        console.log(`kick ${victim}`)
+        console.log('kick', victim)
       }
       else {
         socket.broadcast.emit('kick-declined', victim)
-        console.log(`mercy ${victim}`)
+        console.log('mercy', victim)
       }
       kickCounter = 0
       voices = 0
@@ -83,6 +83,7 @@ io.on('connection', (socket) => {
   })
 
   socket.on('request-master', () => {
+    console.log(users[socket.id], 'requested master', usersArr.find(el => { if ( el.role == 'Dealer' ) return el }))
     socket.emit('recieve-master', usersArr.find(el => { if ( el.role == 'Dealer' ) return el }))
   })
 
@@ -90,27 +91,33 @@ io.on('connection', (socket) => {
     user.role = 'Dealer'
     users[socket.id] = user
     usersArr[user.id] = user
+    console.log(user, 'is set as dealer')
   })
 
   socket.on('request-all-rounds', () => {
+    console.log(users[socket.id], 'requested all issues', issues)
     socket.emit('recieve-all-rounds', issues)
   })
 
   socket.on('set-all-rounds', (rounds) => {
     rounds.map((el) => { el.id = idGenerator(0, rounds) })
     issues = rounds
+    console.log('set all rounds', issues)
   })
 
   socket.on('game-start', () => {
     game.rounds = issues;
     game.players = usersArr;
     game.master = usersArr.find(el => { if ( el.role == 'Dealer' ) return el });
+    console.log('game started', game)
     socket.emit('game-started', game)
     currentIssue = issues[0];
+    console.log('current issue is', currentIssue)
   })
 
   socket.on('user-vote', (vote) => {
     currentIssue.votes.push(vote)
+    console.log(users[socket.id], 'voted', vote)
   })
 
   socket.on('round-end', () => {
@@ -119,26 +126,33 @@ io.on('connection', (socket) => {
       average = +el.vote + average
     })
     average = average / votes.length
-
     game.rounds[currentIssue.id].average = average
     issues.splice(0, 1)
     currentIssue = issues[0]
     socket.emit('round-change', currentIssue)
+    console.log('average is', average)
+    console.log('current issue is', currentIssue)
   })
 
   socket.on('request-all-stats', () => {
     socket.emit('receive-all-stats', game.rounds)
+    console.log('send all stats', game.rounds)
   })
 
   socket.on('request-round-stats', (issue) => {
     const stats = game.rounds.find(el => { if (el.issue == issue) return el})
     socket.emit('recieve-round-stats', stats)
+    console.log('send', issue, 'round stats', stats)
   })
 
-  socket.on('update-lobby-settings', settings => settings = settings)
+  socket.on('update-lobby-settings', newSettings => {
+    settings = newSettings
+    console.log('set settings', settings)
+  })
 
   socket.on('request-lobby-settings', () => {
     socket.emit('recieve-lobby-settings', settings)
+    console.log('send lobby settings', settings)
   })
 
   socket.on('reset', () => {
@@ -156,5 +170,6 @@ io.on('connection', (socket) => {
     issues = []
     currentIssue = null
     settings = null
+    console.log('reset initiated')
   })
 })
