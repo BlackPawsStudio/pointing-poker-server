f1daab5381c82364b7ae9c5fd42b31000e31f47d
BlackPawsStudio <blackpawsstudio@gmail.com>
2021-10-09 14:07:36 +0300

    add extra chat

--- back/chat.js
+++ back/chat.js
@@ -14,38 +14,38 @@ io.on('connection', socket => {
   console.log(`connected`)
 
   //my server part
-  socket.on('connect-user', name => {
+  socket.on('chat-connect-user', name => {
     if(usersArr.includes(name))
       socket.emit('relogin')
     else {
       users[socket.id] = name
       usersArr.push(name) 
-      socket.broadcast.emit('user-connected', name)
+      socket.broadcast.emit('chat-user-connected', name)
       console.log(`${users[socket.id]} connected`)
     }
   })
-  socket.on('message-request', message => {
-    socket.broadcast.emit('message-post', [users[socket.id], message])
+  socket.on('chat-message-request', message => {
+    socket.broadcast.emit('chat-message-post', [users[socket.id], message])
     console.log(`${users[socket.id]} messaged ${message}`)
   })
-  socket.on('get-users-request', () => {
-    socket.emit('send-all-users', usersArr)
+  socket.on('chat-get-users-request', () => {
+    socket.emit('chat-send-all-users', usersArr)
   })
-  socket.on('get-kick-request', () => {
-    socket.emit('kick-users-list', usersArr)
+  socket.on('chat-get-kick-request', () => {
+    socket.emit('chat-kick-users-list', usersArr)
   })
   socket.on('disconnect', () => {
-    socket.broadcast.emit('user-disconnected', users[socket.id])
+    socket.broadcast.emit('chat-user-disconnected', users[socket.id])
     console.log(`${users[socket.id]} disconnected`)
     usersArr.splice(usersArr.indexOf(users[socket.id]), 1)
     delete users[socket.id]
   })
-  socket.on('kick-user', name => {
+  socket.on('chat-kick-user', name => {
     socket.broadcast.emit('kick-offer', name)
     victim = name
     console.log(`${users[socket.id]} wants to kick ${victim}`)
   })
-  socket.on('user-opinion', answ => {
+  socket.on('chat-user-opinion', answ => {
     kickCounter += 1
     if (answ) {
       console.log(`${users[socket.id]} chosen ${answ}`)
@@ -54,11 +54,11 @@ io.on('connection', socket => {
     console.log(`all voices ${kickCounter}, voices agrees ${voices}`)
     if (usersArr.length == kickCounter) {
       if (voices >= usersArr.length / 2) {
-        socket.broadcast.emit('force-disconnect', victim)
+        socket.broadcast.emit('chat-force-disconnect', victim)
         console.log(`kick ${victim}`)
       }
       else {
-        socket.broadcast.emit('kick-declined', victim)
+        socket.broadcast.emit('chat-kick-declined', victim)
         console.log(`mercy ${victim}`)
       }
       kickCounter = 0
@@ -66,9 +66,9 @@ io.on('connection', socket => {
       victim = null
     }
   })
-  socket.on('user-is-typing', () => {
+  socket.on('chat-user-is-typing', () => {
     console.log(`Somebody is typing`)
-    socket.broadcast.emit('this-user-is-typing')
+    socket.broadcast.emit('chat-this-user-is-typing')
   })
 })
 
--- back/index.js
+++ back/index.js
@@ -172,4 +172,63 @@ io.on('connection', (socket) => {
     settings = null
     console.log('reset initiated')
   })
+
+///////////////////////////////////////////////////////////////////////////////////
+
+  socket.on('chat-connect-user', name => {
+    if(usersArr.includes(name))
+      socket.emit('relogin')
+    else {
+      users[socket.id] = name
+      usersArr.push(name) 
+      socket.broadcast.emit('chat-user-connected', name)
+      console.log(`${users[socket.id]} connected`)
+    }
+  })
+  socket.on('chat-message-request', message => {
+    socket.broadcast.emit('chat-message-post', [users[socket.id], message])
+    console.log(`${users[socket.id]} messaged ${message}`)
+  })
+  socket.on('chat-get-users-request', () => {
+    socket.emit('chat-send-all-users', usersArr)
+  })
+  socket.on('chat-get-kick-request', () => {
+    socket.emit('chat-kick-users-list', usersArr)
+  })
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('chat-user-disconnected', users[socket.id])
+    console.log(`${users[socket.id]} disconnected`)
+    usersArr.splice(usersArr.indexOf(users[socket.id]), 1)
+    delete users[socket.id]
+  })
+  socket.on('chat-kick-user', name => {
+    socket.broadcast.emit('kick-offer', name)
+    victim = name
+    console.log(`${users[socket.id]} wants to kick ${victim}`)
+  })
+  socket.on('chat-user-opinion', answ => {
+    kickCounter += 1
+    if (answ) {
+      console.log(`${users[socket.id]} chosen ${answ}`)
+      voices += 1
+    }
+    console.log(`all voices ${kickCounter}, voices agrees ${voices}`)
+    if (usersArr.length == kickCounter) {
+      if (voices >= usersArr.length / 2) {
+        socket.broadcast.emit('chat-force-disconnect', victim)
+        console.log(`kick ${victim}`)
+      }
+      else {
+        socket.broadcast.emit('chat-kick-declined', victim)
+        console.log(`mercy ${victim}`)
+      }
+      kickCounter = 0
+      voices = 0
+      victim = null
+    }
+  })
+  socket.on('chat-user-is-typing', () => {
+    console.log(`Somebody is typing`)
+    socket.broadcast.emit('chat-this-user-is-typing')
+  })
 })
